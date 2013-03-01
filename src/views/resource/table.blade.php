18ac353255aaf7bf1fe265e8ea2ce2501c3922b9
Jim Wardlaw <jim.wardlaw@gmail.com>
2013-03-01 23:01:41 +0000

    table now handles anonymous functions in columns array

--- src/views/resource/table.blade.php
+++ src/views/resource/table.blade.php
@@ -8,15 +8,19 @@
 @section('content')
 <h1>{{ $title }}</h1>
 <nav>
-    {{{ $createButton }}}
+    {{ $createButton }}
 </nav>
 <table class="table table-bordered table-striped table-hover">
     <caption>Displaying all {{ $plural }}</caption>
     <thead>
         <tr>
             <th><input id="row-all" name="ids-all" type="checkbox" /></th>
-        @foreach ($columns as $c)
-            <th>{{ $c->label }}</th>
+         @foreach ($columns as $k => $v)
+            @if (is_callable($v))
+            <th>{{ $k }}</th>
+            @elseif(is_object($v))
+            <th>{{ $v->label }}</th>
+            @endif
         @endforeach
             <th>Actions</th>
         </tr>
@@ -25,9 +29,13 @@
         @foreach ($resource as $r)
         <tr>
             <td><input id="row-{{ $r->id }}" name="ids[]" type="checkbox" value="{{ $r->id }}"/></td>
-            @foreach ($columns as $c)
-            <td><label for="row-{{ $r->id }}">{{ $r->{$c->name} }}</label></td>
-            @endforeach
+        @foreach ($columns as $k => $v)
+            @if (is_object($v) && !($v instanceof Closure))
+                <td><label for="row-{{ $r->id }}">{{ $r->{$v->name} }}</label></td>
+            @else
+            <td><label for="row-{{ $r->id }}">{{ call_user_func($v, $r) }}</label></td>
+            @endif
+        @endforeach
             <td>
                 <a class="btn" href="{{ URL::to('admin/'.$controller.'/'.$r->id) }}">View</a>
                 <a class="btn" href="{{ URL::to('admin/'.$controller.'/'.$r->id.'/edit') }}">Edit</a>
