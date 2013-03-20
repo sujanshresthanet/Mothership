69c3964a987e50389ed13c271cd30c0a7b003f05
Jim Wardlaw <jim.wardlaw@gmail.com>
2013-03-20 22:45:16 +0000

    Remove Basset Package support

--- src/views/layouts/main.blade.php
+++ src/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
         <title>@yield('title')</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.0/css/bootstrap-combined.min.css" rel="stylesheet">
-        <?=Basset\Facades\Basset::show('mothership.css'); ?>
+        <link href="{{ URL::to('styles/css/mothership.css') }}" rel="stylesheet" />
         <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
         <!--[if lt IE 9]>
           <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -21,6 +21,5 @@
                 @yield('content')
             </div>
         </div>
-        <?=Basset\Facades\Basset::show('mothership-footer.js'); ?>
     </body>
 </html>
