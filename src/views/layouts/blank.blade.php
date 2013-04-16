4a1c33a57fdbcfe8686fb92780c287f03af70eb2
Jim Wardlaw <jim.wardlaw@gmail.com>
2013-04-16 17:22:59 +0100

    Added css into the package

--- src/views/layouts/blank.blade.php
+++ src/views/layouts/blank.blade.php
@@ -4,7 +4,7 @@
         <title>@yield('title')</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.0/css/bootstrap-combined.min.css" rel="stylesheet">
-        <link href="{{ URL::to('styles/css/mothership.css') }}" rel="stylesheet" />
+        <link href="{{ URL::to('packages/stwt/mothership/styles/css/mothership.css') }}" rel="stylesheet" />
     </head>
     <body>
         <header class="app-header" id="app-header">
--- src/views/layouts/main.blade.php
+++ src/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
         <title>@yield('title')</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.0/css/bootstrap-combined.min.css" rel="stylesheet">
-        <link href="{{ URL::to('styles/css/mothership.css') }}" rel="stylesheet" />
+        <link href="{{ URL::to('packages/stwt/mothership/styles/css/mothership.css') }}" rel="stylesheet" />
         <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
         <!--[if lt IE 9]>
           <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
