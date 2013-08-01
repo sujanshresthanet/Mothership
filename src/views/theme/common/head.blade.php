eb1e2f134373a2b0145cade833791efaad379e0c
Jim Wardlaw <jim.wardlaw@gmail.com>
2013-08-01 15:16:03 +0100

    Added bootstrap v3.0 style

--- src/views/theme/common/head.blade.php
+++ src/views/theme/common/head.blade.php
@@ -3,9 +3,6 @@
     <head>
         <title>{{ $title }}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!--<link href="{{ URL::to($appStyle) }}" rel="stylesheet" />-->
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css">
-        <link href="{{ URL::to('assets/styles/css/bootstrap-glyphicons.css') }}" rel="stylesheet" />
+        <link href="{{ URL::to($appStyle) }}" rel="stylesheet" />
     </head>
     <body>
