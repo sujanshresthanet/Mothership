e8d52f9c565af057bd20453857a9cea12e2f8055
Jim Wardlaw <jim.wardlaw@gmail.com>
2013-08-26 20:17:31 +0100

    update login

--- src/views/theme/home/login.blade.php
+++ src/views/theme/home/login.blade.php
@@ -1,36 +1,40 @@
 @extends('mothership::theme.layouts.base')
 
+@section('title')
+    Please Login
+@stop
+
+
 @section('body')
-
-<div class="container" id="container">   
-    {{ Stwt\Mothership\Messages::getHtml() }}
-    <div class="row">
-        <div class="col-lg-4 col-offset-4">
-            <header class="page-header">
-                <h1>{{ $title }}</h1>
-            </header>
-            <form action="{{ URL::to('admin/login') }}" class="form-horizontal" method="post">
-              <div class="form-group">
-                <label for="email" class="col-lg-3 control-label">Email</label>
-                <div class="col-lg-9">
-                  <input type="text" class="form-control" id="email" placeholder="Email" name="email" />
-                </div>
+  <main class="sign-in" id="sign-in">
+      <form action="{{ URL::to('admin/login') }}" class="form-signin" method="POST" style="margin-top:30px;" role="form">
+          <legend>Please Sign In</legend>
+          <div class="form-group">
+              <label for="inputEmail1" class="col-lg-2 control-label">Email</label>
+              <div class="col-lg-10">
+                  <input type="email" name="email" class="form-control" id="inputEmail1" placeholder="Email">
               </div>
-              <div class="form-group">
-                <label for="inputPassword" class="col-lg-3 control-label">Password</label>
-                <div class="col-lg-9">
-                  <input type="password" class="form-control" id="password" placeholder="Password" name="password">
-                  <div class="checkbox">
-                    <label>
-                      <input type="checkbox"> Remember me
-                    </label>
+          </div>
+          <div class="form-group">
+              <label for="inputPassword1" class="col-lg-2 control-label">Password</label>
+              <div class="col-lg-10">
+                  <input type="password" name="password" class="form-control" id="inputPassword1" placeholder="Password">
+              </div>
+          </div>
+          <div class="form-group">
+              <div class="col-lg-offset-2 col-lg-10">
+                <div class="checkbox">
+                      <label>
+                          <input type="checkbox"> Remember me
+                      </label>
                   </div>
-                  <button type="submit" class="btn btn-default">Sign in</button>
-                </div>
               </div>
-            </form>
-        </div>
-    </div>
-</div>
-
+          </div>
+          <div class="form-group">
+              <div class="col-lg-offset-2 col-lg-10">
+                  <button type="submit" class="btn btn-success btn-lg"><span class="glyphicon glyphicon-ok"></span> Sign in</button>
+              </div>
+          </div>
+      </form>
+  </main>
 @stop
