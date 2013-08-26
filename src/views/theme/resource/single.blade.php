52760a7a4cc51598f6826816751f7fe3f34c8a4b
Jim Wardlaw <jim.wardlaw@gmail.com>
2013-08-26 20:40:33 +0100

    changed back button icon

--- src/views/theme/resource/single.blade.php
+++ src/views/theme/resource/single.blade.php
@@ -6,7 +6,8 @@
     <header class="main-header">
         <h1>{{ $title }}</h1>
         <nav>
-            <a class="pull-right btn" href="{{ mo_index() }}"><i class="icon icon-arrow-left"></i> Back to {{ $plural }}</a>
+            <a class="pull-right btn btn-default" href="{{ mo_index() }}"><span class="glyphicon glyphicon-arrow-left
+"></span> Back to {{ $plural }}</a>
         </nav>
         @include('mothership::theme.common.tabs')
     </header>
