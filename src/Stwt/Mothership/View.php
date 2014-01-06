5c71dd8cdd8c76af2eb57644dab11e8dbce25d98
Jim Wardlaw <jim.wardlaw@gmail.com>
2014-01-06 14:51:44 +0000

    remove view extended methods

--- src/Stwt/Mothership/View.php
+++ src/Stwt/Mothership/View.php
@@ -4,20 +4,5 @@ use Config;
 
 class View extends \Illuminate\Support\Facades\View
 {
-    public static function baseData($data)
-    {
 
-        if (isset($data['resource'])) {
-            $data = Arr::s($data, 'singular', $data['resource']->singular());
-            $data = Arr::s($data, 'plural', $data['resource']->plural());
-        }
-
-        return $data;
-    }
-
-    public static function makeTemplate($path, $data = [])
-    {
-        $data = self::baseData($data);
-        return parent::make($path, $data);
-    }
 }
